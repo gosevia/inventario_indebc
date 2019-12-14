3b442282bf1c17e0703d2f7aee8e6a891521be66
detapia21 <etj_96@hotmail.com>
2019-12-14 00:05:47 -0800

    validacion prestamos null

--- application/views/empleado/prestamos_empleado.php
+++ application/views/empleado/prestamos_empleado.php
@@ -13,25 +13,26 @@
                 </thead>
                 <tbody>
                 <?php 
-
-                foreach($prestamos as $row){
-                    echo "<tr>";
-                    echo "<td>".$row['idPrestamo']."</td>";
-                    echo "<td>".$row['numPrestamo']."</td>";
-                    echo "<td>".$row['fecha_inicio']."</td>";
-                    echo "<td>".$row['fecha_fin']."</td>";
-                    switch($row['status']){
-                        case 0: echo "<td id='estado1'>Concluido</td>"; break;
-                        case 1: echo "<td id='estado2'>Temporal</td>"; break;
-                        case 2: echo "<td id='estado3'>Permanente</tc>"; break; 
+                if($prestamos != null){
+                    foreach($prestamos as $row){
+                        echo "<tr>";
+                        echo "<td>".$row['idPrestamo']."</td>";
+                        echo "<td>".$row['numPrestamo']."</td>";
+                        echo "<td>".$row['fecha_inicio']."</td>";
+                        echo "<td>".$row['fecha_fin']."</td>";
+                        switch($row['status']){
+                            case 0: echo "<td id='estado1'>Concluido</td>"; break;
+                            case 1: echo "<td id='estado2'>Temporal</td>"; break;
+                            case 2: echo "<td id='estado3'>Permanente</tc>"; break; 
+                        }
+                        echo '<td>';
+                        echo form_open('index.php/empleado/detalles_prestamo');
+                        echo "<input type='hidden' id='detalle' name='detalle' value='".$row['idPrestamo']."' />";
+                        echo '<button class="btn btn-outline-primary" type="submit" value=""><i class="fa fa-search" id="submit"></i></button>';
+                        echo form_close();
+                        echo "</td>";
+                        echo "</tr>";           
                     }
-                    echo '<td>';
-                    echo form_open('index.php/empleado/detalles_prestamo');
-                    echo "<input type='hidden' id='detalle' name='detalle' value='".$row['idPrestamo']."' />";
-                    echo '<button class="btn btn-outline-primary" type="submit" value=""><i class="fa fa-search" id="submit"></i></button>';
-                    echo form_close();
-                    echo "</td>";
-                    echo "</tr>";           
                 }
                 ?>
             </tbody>
